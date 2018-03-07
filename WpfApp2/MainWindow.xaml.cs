0b060357d50cf85e0842923acf92702aa4278d4b
pibop0115001 <pibop0115001@CL329-05>
2018-03-07 11:02:21 +0700

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BibleThump knigi = new BibleThump() { izdatelstvo = "Mr.Block_developers", Address_izdania = "Красный проспект", book_Name = "Программирование для чайников", Age_izdania = 2018, avtor = "Блок.И.Н" };
+
+        ObservableCollection<BibleThump> students = new ObservableCollection<BibleThump>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            listStudents.ItemsSource = students;
+
+            DataContext = knigi;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Имя: {knigi.book_Name} Возраст: {knigi.Age_izdania}");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            students.Add(knigi);
+            knigi = new BibleThump();
+            DataContext = knigi;
+        }
+    }
+}
---
+++ WpfApp2/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class BibleThump
+    {
+        //string _group;
+        public string avtor { get; set; }
+        public string izdatelstvo { get; set; }
+        public string book_Name { get; set; }
+        public int Age_izdania { get; set; }
+        public string Address_izdania { get; set; }
+        //public string Group { get { return _group; } set { _group = value; } }
+        public override string ToString()
+        {
+            return $"Книжка {book_Name},  {Age_izdania} Года издания";
+        }
+    }
+}
